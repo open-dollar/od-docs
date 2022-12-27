bcc2877fad9c44da084b2185f30c92d1bab3054e
Jacob Habib <47253537+jahabeebs@users.noreply.github.com>
2022-12-27 13:13:01 -0500

    added .docusaurus to gitignore, added yarn lock, deleted .docusaurus (#5)

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/jsch/Desktop/docs/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/jsch/Desktop/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/jsch/Desktop/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/jsch/Desktop/docs/src/css/custom.css'),
-];
