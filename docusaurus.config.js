638a9efe237b25a5fe4ad24cd2ccd3de3d6a0f6f
alekseevaiana <alekseevadev@gmail.com>
2023-08-02 14:44:53 -0700

    add the most structure of a sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Keyp Docs",
+  title: "Open Dollar Docs",
   tagline: "The no brainer onboarding stack for web3.",
   url: "https://docs.UseKeyp.com",
   baseUrl: "/",
