913bb155e498dea86468c239be133c5a3e138f18
jahabeebs <jahgoreds@gmail.com>
2024-01-16 14:41:44 -0500

    disable edit this page on gh

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,6 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/open-dollar/od-docs/tree/main",
           sidebarCollapsed: false,
           sidebarCollapsible: false,
           remarkPlugins: [remarkMath],
